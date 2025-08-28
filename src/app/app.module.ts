bc9703eac065db9d61edcc3b088d29f588b51613
uriasvitor <uriasvitor@gmail.com>
2025-08-28 16:11:00 -0300

    refactor: update dependencies and improve sharing functionality
    - Removed deprecated @angular/http and replaced with @angular/common/http.
    - Updated package.json to remove unused Capacitor dependencies and added cordova-plugin-whitelist.
    - Enhanced sharing functionality in CarteirinhaPage and CarteirinhasPage to use Web Share API where available, falling back to SocialSharing plugin for Android.
    - Cleaned up code for better readability and maintainability.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { HttpModule, JsonpModule } from '@angular/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -9,6 +9,7 @@ import { AssociadoProvider } from 'src/providers/associado/associado';
 import { ControleAcessoProvider } from 'src/providers/controle-acesso/controle-acesso';
 import { UsuarioProvider } from 'src/providers/usuario/usuario';
 import { FileOpener } from "@ionic-native/file-opener/ngx";
+import { SocialSharing } from "@ionic-native/social-sharing/ngx";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,12 +17,12 @@ import { PdfViewerService } from './services/pdf-viewer.service';
 import { FileTransfer } from "@ionic-native/file-transfer/ngx";
 import { File } from "@ionic-native/file/ngx";
 import { ConsultaProvider } from 'src/providers/consulta/consulta';
+import { DefaultRequestOptions } from 'src/core/defaultRequestOptions';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-    HttpModule,
-    JsonpModule,
+    HttpClientModule,
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule
@@ -35,8 +36,10 @@ import { ConsultaProvider } from 'src/providers/consulta/consulta';
     FileTransfer,
     FileOpener,
     File,
+    SocialSharing,
     PdfViewerService,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: DefaultRequestOptions, multi: true }
   ],
   bootstrap: [AppComponent],
 })
